317ee099a945074f3e503070418311c72c78b38a
KaneTheDev <lehoaituankhang@gmail.com>
2025-11-10 18:04:10 +0700

    Demo Server/Client

---
+++ Demo_Client/Demo_Client/Demo_Client.cpp
@@ -0,0 +1,88 @@
+﻿#include "stdafx.h"
+#include "Demo_Client.h"
+#include "afxsock.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(NULL);
+
+	if (hModule != NULL)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		{
+			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: code your application's behavior here.
+			AfxSocketInit(NULL);
+			CSocket client;
+			
+			client.Create();
+			client.Connect(_T("127.0.0.1"), 4567);
+			
+			string username = "Tuan Khang";
+			string message, reply;
+			int length;
+
+			while (true) {
+				
+				// gửi
+				cout << username << ": ";
+				getline(cin, message, '\n');
+
+				if (message == "exit")
+					break;
+
+				length = message.size();
+				client.Send(&length, sizeof(length), 0);
+
+				client.Send(message.c_str(), length, 0);
+
+				// nhận
+				int bytesReceived = client.Receive(&length, sizeof(length), 0);
+				if (bytesReceived <= 0) {
+					std::cout << "Server disconnected.\n";
+					break;
+				}
+
+				vector<char> buffer(length + 1);
+				int totalReceived = 0;
+				while (totalReceived < length) {
+					int chunk = client.Receive(buffer.data() + totalReceived, length - totalReceived, 0);
+					if (chunk <= 0) {
+						cout << "Connection lost.\n";
+						break;
+					}
+					totalReceived += chunk;
+				}
+				buffer[length] = '\0';
+
+				reply = buffer.data();
+				cout << "Server: " << reply << "\n";
+			}
+		}
+	}
+	else
+	{
+		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
---
+++ Demo_Server/Demo_Server/Demo_Server.cpp
@@ -0,0 +1,105 @@
+﻿// Demo_Server.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Demo_Server.h"
+#include "afxsock.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CWinApp theApp;
+
+using namespace std;
+
+DWORD WINAPI function_cal(LPVOID arg)
+{
+	SOCKET* hConnected = (SOCKET*) arg;
+	CSocket mysock;
+	//Chuyen ve lai CSocket
+	mysock.Attach(*hConnected);
+	
+	int length;
+	string message;
+
+	while (true) {
+		
+		// nhận
+		mysock.Receive(&length, sizeof(length), 0);
+
+		vector<char> buffer(length + 1);
+		int total = 0;
+		while (total < length) {
+			int chunk = mysock.Receive(buffer.data() + total, length - total, 0);
+			if (chunk <= 0) break;
+			total += chunk;
+		}
+		buffer[length] = '\0';
+		message = buffer.data();
+
+		// phản hồi
+		string reply = "Received " + message;
+		length = reply.size();
+		mysock.Send(&length, sizeof(length), 0);
+		mysock.Send(reply.c_str(), length, 0);
+	}
+	
+	delete hConnected;
+	return 0;
+}
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(NULL);
+
+	if (hModule != NULL)
+	{
+		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		{
+			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+			nRetCode = 1;
+		}
+		else
+		{
+			AfxSocketInit(NULL);
+			CSocket server, client;
+
+			if (!server.Create(4567)) {
+				cout << "Cannot create server socket!" << '\n';
+				return 1;
+			}
+
+			cout << "Waiting for client..." << '\n';
+			server.Listen();
+
+			if (server.Accept(client)) {
+				cout << "Client connected successfully!" << '\n';
+
+				SOCKET* hConnected = new SOCKET();
+				*hConnected = client.Detach();
+
+				function_cal(hConnected);
+
+				delete hConnected;
+			}
+			else {
+				cout << "Failed to accept connection!" << '\n';
+			}
+		}
+	}
+	else
+	{
+		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
+
+

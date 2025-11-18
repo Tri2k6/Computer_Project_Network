94caed965f517c5d6c0f0f229964d3b1e9b32b6a
KaneTheDev <lehoaituankhang@gmail.com>
2025-11-18 17:35:49 +0700

    demo.cpp Modified

--- demo.cpp
+++ demo.cpp
@@ -11,13 +11,17 @@
 #include <io.h>
 
 int main() {
-    // chương trình không hiểu tên ứng dụng và ngắt stream nên phải thêm
+
+    int i = 0;
+
+    #if defined(_WIN32)
+    // Windows version
+    // chương trình không hiểu tên ứng dụng tiếng việt và ngắt stream nên phải đổi chế độ in/out
     _setmode(_fileno(stdout), _O_U16TEXT);
     _setmode(_fileno(stdin),  _O_U16TEXT);
 
     WinProcessController pc;
     WinAppController ac;
-    int i = 0;
 
     pc.listProcesses();
 
@@ -48,6 +52,44 @@ int main() {
         std::wcout << L"Shut down!\n";
     else
         std::wcout << L"Not found!\n";
+
+    #elif defined(__APPLE__)
+    // macOS version
+    MacProcessController pc;
+    MacAppController ac;
+
+    pc.listProcesses();
+
+    std::cout << "\nSelect a process: ";
+    std::cin >> i;
+
+    if (pc.stopProcess(pc.getProcess(i)))
+        std::cout << "Killed!\n";
+    else
+        std::cout << "Not found!\n";
+
+    // Sleep không có sẵn trong macOS, dùng usleep thay thế (micro giây)
+    usleep(3000 * 1000);  // 3000 ms = 3s
+
+    ac.listApps();
+
+    std::cout << "\nSelect an app: ";
+    std::cin >> i;
+
+    if (ac.startApp(ac.getApp(i)))
+        std::cout << "Activated!\n";
+    else
+        std::cout << "Not found!\n";
+
+    std::cout << "\nClose an app: ";
+    std::cin >> i;
+
+    if (ac.stopApp(ac.getApp(i)))
+        std::cout << "Shut down!\n";
+    else
+        std::cout << "Not found!\n";
+
+    #endif
 
     return 0;
 }

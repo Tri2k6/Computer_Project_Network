dabe99a2cd69b95633e5b14cb06c50481f97c129
NgocPhan <phanlanngoc567@gmail.com>
2025-12-03 03:14:12 +0700

    sync client with server

--- client/include/Message.hpp
+++ client/include/Message.hpp
@@ -9,22 +9,26 @@ using json = nlohmann::json;
 
 class Message {
 public:
-    std::string cmd;
-    std::string data;
-    std::string from;
+    std::string type;
+    json data;
     std::string to;
+    std::string from;
 
     Message() = default;
-    Message(const std::string& c, const std::string& d,
+    Message(const std::string& c, const json& d,
             const std::string& f = "", const std::string& t = "")
-            : cmd(c), data(d), from(f), to(t) {}
+            : type(c), data(d), from(f), to(t) {}
     
     std::string serialize() const {
         json j;
-        j["cmd"] = cmd;
+        j["type"] = type;
         j["data"] = data;
+        // both valid for string or object
         if (!from.empty()) j["from"] = from;
         if (!to.empty()) j["to"] = to;
+        j.dump(-1, ' ', false, json::error_handler_t::replace);
+        // try not to crash when meet invalid char
+        // picture should be base64
         return j.dump();
     }
 
@@ -32,14 +36,19 @@ public:
         Message msg;
         try {
             auto j = json::parse(str);
-            if (j.contains("cmd")) msg.cmd = j["cmd"];
+            msg.type = j.value("type", "unknown");
             if (j.contains("data")) msg.data = j["data"];
-            if (j.contains("from")) msg.from = j["from"];
-            if (j.contains("to")) msg.to = j["to"];
+            msg.from = j.value("from", "");
+            msg.to = j.value("to", "");
         } catch (const std::exception& e) {
             std::cerr << "[Message] JSON parse error: " << e.what() << "\n";
         }
         
         return msg;
+    }
+
+    std::string getDataString() const {
+        if (data.is_string()) return data.get<std::string>();
+        return data.dump(); //from object to string
     }
 };
